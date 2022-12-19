9c72292fb8b076d55b170607a26749cd4f275493
Jeong77777 <ttletter7@naver.com>
2022-12-19 22:18:26 +0900

    connect 함수에 assert 활용 - MainWindow 클래스(ManagementProgram)

--- ManagementProgram/mainwindow.cpp
+++ ManagementProgram/mainwindow.cpp
@@ -25,74 +25,74 @@ MainWindow::MainWindow(QWidget *parent)
 
     // 고객 관리 객체 생성
     clientForm = new ClientManagerForm(this);
-    connect(clientForm, SIGNAL(destroyed()),
-            clientForm, SLOT(deleteLater()));
+    assert(connect(clientForm, SIGNAL(destroyed()),
+            clientForm, SLOT(deleteLater())));
     clientForm->setWindowTitle(tr("Client Info"));
 
     // 제품 관리 객체 생성
     productForm = new ProductManagerForm(this);
-    connect(productForm, SIGNAL(destroyed()),
-            productForm, SLOT(deleteLater()));
+    assert(connect(productForm, SIGNAL(destroyed()),
+            productForm, SLOT(deleteLater())));
     productForm->setWindowTitle(tr("Product Info"));
 
     // 주문 관리 객체 생성
     // 주문 관리 객체에서는 고객, 제품 검색 다이얼로그를 사용한다.
     orderForm = new OrderManagerForm(this, clientDialog, productDialog);
-    connect(orderForm, SIGNAL(destroyed()),
-            orderForm, SLOT(deleteLater()));
+    assert(connect(orderForm, SIGNAL(destroyed()),
+            orderForm, SLOT(deleteLater())));
     orderForm->setWindowTitle(tr("Order Info"));
 
     // 채팅 서버 객체 생성
     chatForm = new ChatServerForm(this);
-    connect(chatForm, SIGNAL(destroyed()),
-            chatForm, SLOT(deleteLater()));
+    assert(connect(chatForm, SIGNAL(destroyed()),
+            chatForm, SLOT(deleteLater())));
     chatForm->setWindowTitle(tr("Chat server"));
 
     /* 객체 간 signal과 slot을 connect */
     // 고객 검색 다이얼로그에서 검색어를 고객 관리 객체로 전달해줌
-    connect(clientDialog, SIGNAL(sendWord(QString)), \
-            clientForm, SLOT(receiveWord(QString)));
+    assert(connect(clientDialog, SIGNAL(sendWord(QString)), \
+            clientForm, SLOT(receiveWord(QString))));
     // 고객 관리 객체에서 검색 결과를 고객 검색 다이얼로그로 전달해줌
-    connect(clientForm, SIGNAL(sendClientToDialog(int,QString,QString,QString)), \
-            clientDialog, SLOT(receiveClientInfo(int,QString,QString,QString)));
+    assert(connect(clientForm, SIGNAL(sendClientToDialog(int,QString,QString,QString)), \
+            clientDialog, SLOT(receiveClientInfo(int,QString,QString,QString))));
 
     // 제품 검색 다이얼로그에서 검색어를 제품 관리 객체로 전달해줌
-    connect(productDialog, SIGNAL(sendWord(QString)), \
-            productForm, SLOT(receiveWord(QString)));
+    assert(connect(productDialog, SIGNAL(sendWord(QString)), \
+            productForm, SLOT(receiveWord(QString))));
     // 제품 관리 객체에서 검색 결과를 제품 검색 다이얼로그로 전달해줌
-    connect(productForm, SIGNAL(sendProductToDialog(int,QString,QString,int,int)), \
-            productDialog, SLOT(receiveProductInfo(int,QString,QString,int,int)));
+    assert(connect(productForm, SIGNAL(sendProductToDialog(int,QString,QString,int,int)), \
+            productDialog, SLOT(receiveProductInfo(int,QString,QString,int,int))));
 
     // 주문 관리 객체에서 정보를 받아올 고객의 id를 고객 관리 객체로 전달해줌
-    connect(orderForm, SIGNAL(sendClientId(int)), \
-            clientForm, SLOT(receiveId(int)));
+    assert(connect(orderForm, SIGNAL(sendClientId(int)), \
+            clientForm, SLOT(receiveId(int))));
     // 고객 관리 객체에서 고객의 정보를 주문 관리 객체로 전달해줌
-    connect(clientForm, SIGNAL(sendClientToOrderManager(int,QString,QString,QString)), \
-            orderForm, SLOT(receiveClientInfo(int,QString,QString,QString)));
+    assert(connect(clientForm, SIGNAL(sendClientToOrderManager(int,QString,QString,QString)), \
+            orderForm, SLOT(receiveClientInfo(int,QString,QString,QString))));
     // 주문 관리 객체에서 정보를 받아올 제품의 id를 제품 관리 객체로 전달해줌
-    connect(orderForm, SIGNAL(sendProductId(int)), \
-            productForm, SLOT(receiveId(int)));
+    assert(connect(orderForm, SIGNAL(sendProductId(int)), \
+            productForm, SLOT(receiveId(int))));
     // 제품 관리 객체에서 제품의 정보를 주문 관리 객체로 전달해줌
-    connect(productForm, SIGNAL(sendProductToManager(int,QString,QString,int,int)), \
-            orderForm, SLOT(receiveProductInfo(int,QString,QString,int,int)));
+    assert(connect(productForm, SIGNAL(sendProductToManager(int,QString,QString,int,int)), \
+            orderForm, SLOT(receiveProductInfo(int,QString,QString,int,int))));
 
     // 고객 관리 객체에서 고객의 정보를 채팅 서버 객체로 전달해줌
-    connect(clientForm, SIGNAL(sendClientToChatServer(int,QString)), \
-            chatForm, SLOT(addClient(int,QString)));
+    assert(connect(clientForm, SIGNAL(sendClientToChatServer(int,QString)), \
+            chatForm, SLOT(addClient(int,QString))));
 
     // 고객 관리 객체에서 mainwindow의 status bar에 메시지를 표시해줌
-    connect(clientForm, SIGNAL(sendStatusMessage(QString,int)), \
-            this->statusBar(), SLOT(showMessage(const QString &,int)));
+    assert(connect(clientForm, SIGNAL(sendStatusMessage(QString,int)), \
+            this->statusBar(), SLOT(showMessage(const QString &,int))));
     // 제품 관리 객체에서 mainwindow의 status bar에 메시지를 표시해줌
-    connect(productForm, SIGNAL(sendStatusMessage(QString,int)), \
-            this->statusBar(), SLOT(showMessage(const QString &,int)));
+    assert(connect(productForm, SIGNAL(sendStatusMessage(QString,int)), \
+            this->statusBar(), SLOT(showMessage(const QString &,int))));
     // 주문 관리 객체에서 mainwindow의 status bar에 메시지를 표시해줌
-    connect(orderForm, SIGNAL(sendStatusMessage(QString,int)), \
-            this->statusBar(), SLOT(showMessage(const QString &,int)));
+    assert(connect(orderForm, SIGNAL(sendStatusMessage(QString,int)), \
+            this->statusBar(), SLOT(showMessage(const QString &,int))));
 
     // 주문 관리 객체에서 제품 관리 객체를 통해 재고수량을 변경함
-    connect(orderForm, SIGNAL(sendStock(int,int)), \
-            productForm, SLOT(setStock(int,int)));
+    assert(connect(orderForm, SIGNAL(sendStock(int,int)), \
+            productForm, SLOT(setStock(int,int))));
 
     /* Mdi Area 설정 */
     QMdiSubWindow *cw = ui->mdiArea->addSubWindow(clientForm);
